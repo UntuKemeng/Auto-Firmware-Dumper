8fc3511a53915230f6df99ebcdff2f7991ceef36
UntuKemeng <jarbull87@gmail.com>
2025-02-01 16:25:40 +0700

    DUMPRX

--- dumprx.sh
+++ dumprx.sh
@@ -10,11 +10,11 @@ if [[ ${UTG} == true ]] then
     msg Firmware Dump Will Be Posted On Github
     echo ${GTOKEN} >> .github_token
     echo ${GTO} >> .github_orgname
+    echo ${TGT} >> .tg_token
+    echo ${CTI} >> .tg_chat
 else
     echo ""
 fi
-echo ${TGT} >> .tg_token
-echo ${CTI} >> .tg_chat
 git config --global user.name ${UN}
 git config --global user.email ${UEM}
 bash dumper.sh ${FUR}
